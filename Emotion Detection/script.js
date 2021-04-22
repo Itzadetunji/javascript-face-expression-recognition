26b166b9fd27d7d8200d6c0cabd709f4cac189e9
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-04-22 12:42:24 +0100

    Updated script.js

--- Emotion Detection/script.js
+++ Emotion Detection/script.js
@@ -1,5 +1,12 @@
 const video = document.getElementById("video");
 
+Promise.all([
+    faceapi.nets.tnyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.tnyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.tnyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.tnyFaceDetector.loadFromUri('/models')
+])
+
 function startVideo(){
     navigator.getUserMedia(
             { video: {} },
